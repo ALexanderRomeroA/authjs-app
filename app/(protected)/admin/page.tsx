8ca04f0ebef2d0f124bccfb59201d1871004fd47
Alexander Romero Arandia <alexanderromeroarandia@gmail.com>
2024-09-21 12:16:44 -0400

    Admin page in process

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSucces } from "@/components/FormSuccess";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useCurrentRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
+
+const AdminPage = () =>{
+
+    const role = useCurrentRole();
+
+    return(
+        <Card className="w-[600px]">
+            <CardHeader>
+                <p className="text-2xl font-semibold text-center">
+                    🔑Admin
+                </p>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <RoleGate allowedRole={UserRole.ADMIN}>
+                    <FormSucces message="You are allowed to se this"/>
+                </RoleGate>
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">
+                        Admin-only API ROUTE
+                    </p>
+                    <Button>
+                        Click to test
+                    </Button>
+                </div>
+                <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+                    <p className="text-sm font-medium">
+                        Admin-only server action
+                    </p>
+                    <Button>
+                        Click to test
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default AdminPage
